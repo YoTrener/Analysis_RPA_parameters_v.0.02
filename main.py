19370dd23d601266f456b1991599f6277de6fa3e
YoTrener <pavel.demin92@yandex.ru>
2023-04-08 20:26:03 +0700

    UPD Изменен основной код, добавлена возможность переключения между вкладками ВАХ и Векторной диаграммой. Добавлена возможность сохранения данных таблицы по ВАХ.

--- main.py
+++ main.py
@@ -1,58 +1,55 @@
 # -*- coding: utf-8 -*-
 
-
 import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt
-from mywindow import Ui_MainWindow
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-import numpy as np
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QTabWidget, QMenuBar,
+                             QMenu, QAction, QFileDialog)
+
+from VoltAmpereCharacteristic import VoltAmpereCharacteristicWindow
+from VectorDiagram import VectorDiagramWindow
 
 
-class Window(QtWidgets.QMainWindow):
+class MainApp(QMainWindow):
     def __init__(self):
-        super(Window, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.ui.chartWidget.addWidget(self.toolbar)
-        self.ui.chartWidget.addWidget(self.canvas)
-        self.ui.pushButton.clicked.connect(self.plot) # Подключаем кнопку к построению графика
+        super().__init__()
 
-    # Получаем значения, введенные в таблицу пользователем
-    def getData(self):
-        rows = self.ui.tableWidget.rowCount()
-        cols = self.ui.tableWidget.columnCount()
-        table_data = []
-        for i_row in range(rows):
-            tmp = []
-            for i_col in range(cols):
-                try:
-                    tmp.append(self.ui.tableWidget.item(i_row, i_col).text().replace(',', '.'))
-                except:
-                    tmp.append('No Data')
-            table_data.append(tmp)
-        return table_data
+        self.menubar = QMenuBar(self)
+        self.setMenuBar(self.menubar)
+        self.file_menu = QMenu('Файл', self)
+        self.menubar.addMenu(self.file_menu)
 
-    # Метод для создания графика на основании полученных значений от пользователя
-    def plot(self):
-        table_list = self.getData()
-        data_dict = {}
-        for i in table_list:
-            data_dict[i[0]] = i[1:]
-        df = pd.DataFrame(data=data_dict, index=['0.5', '10P'], dtype='float64').transpose()
-        sns.lineplot(df, markers=True)
-        self.canvas.draw()
+        self.action_save_to_excel = QAction('Сохранить в Excel')
+        self.action_save_to_excel.triggered.connect(self.import_to_excel)
+        self.file_menu.addAction(self.action_save_to_excel)
+
+        self.tab_widget = QTabWidget()
+        self.setCentralWidget(self.tab_widget)
+
+        self.first_window = VoltAmpereCharacteristicWindow()
+        self.second_window = VectorDiagramWindow()
+
+        self.tab_widget.addTab(self.first_window, 'Вольт-Амперная характеристика')
+        self.tab_widget.addTab(self.second_window, 'Векторная диаграмма')
+
+        self.setWindowTitle('Анализ параметров РЗА')
+        self.setGeometry(100, 100, 950, 600)
+
+    def import_to_excel(self):
+        file_name, _ = QFileDialog.getSaveFileName(self, 'Сохранить как', '', 'Excel Files (*.xlsx);;All Files (*)')
+        if not file_name:
+            return
+
+        # Если имя файла не заканчивается на ".xlsx", добавляем это расширение
+        if not file_name.endswith('.xlsx'):
+            file_name += '.xlsx'
+
+        # Получаем данные из таблицы VoltAmpereCharacteristicWindow
+        df = self.first_window.get_table_data()
+        # Сохраняем данные в Excel-файл
+        df.to_excel(file_name, index=False)
 
 
-app = QtWidgets.QApplication(sys.argv)
-application = Window()
-application.show()
-
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_app = MainApp()
+    main_app.show()
+    sys.exit(app.exec_())
